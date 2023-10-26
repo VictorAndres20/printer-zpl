fdd4efdbcc8dfade327427f6702415424fe5cbe8
victorandres20 <vapedraza1706@gmail.com>
2023-10-25 20:53:29 -0500

    Change reload

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ def read_root():
 set_api_routes(app)
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8989, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8989, reload=False)
