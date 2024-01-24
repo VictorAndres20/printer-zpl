e8291a42717deb46dcce0f2f0c90567a0972c862
victorandres20 <vapedraza1706@gmail.com>
2024-01-24 15:47:39 -0500

    ready

--- src/endpoints/print_entry_point.py
+++ src/endpoints/print_entry_point.py
@@ -19,19 +19,23 @@ router = APIRouter(
 async def print_box(request: OrderDetailRequest) -> Response:
     """Function printing python version."""
     return controller.print_order_detail(request)
+    # return Response(ok=True)
 
 
 @router.post("/print-rol")
 async def print_roller(request: RolerRequest) -> Response:
     """Function printing python version."""
     return controller.print_rol(request)
+    # return Response(ok=True)
 
 @router.post("/print-nalsani-rol")
 async def print_roller_nalsani(request: OrderDetailEanRequest) -> Response:
     """Function printing python version."""
     return controller.print_order_nalsani_detail(request)
+    # return Response(ok=True)
 
 @router.post("/print-eliot-rol")
 async def print_roller_eliot(request: OrderDetailEanRequest) -> Response:
     """Function printing python version."""
     return controller.print_order_eliot_detail(request)
+    # return Response(ok=True)
