d34f97b860fcecdf168358945918ea619e8074f5
victorandres20 <vapedraza1706@gmail.com>
2024-11-30 17:32:10 -0500

    chore: added env part for get ok response in env development

--- src/endpoints/print_entry_point.py
+++ src/endpoints/print_entry_point.py
@@ -3,6 +3,9 @@ from fastapi import APIRouter
 from src.controllers.printer_controller import PrinterController
 from src.request.request import OrderDetailRequest, RolerRequest, OrderDetailEanRequest
 from src.request.response import Response
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 controller = PrinterController()
 
@@ -16,24 +19,34 @@ router = APIRouter(
 
 @router.post("/print")
 async def print_box(request: OrderDetailRequest) -> Response:
+    if config['ENV'] == 'dev':
+        return Response(ok=True, message='Process Finished', data={})
     return controller.print_order_detail(request)
 
 
 @router.post("/print-rol")
 async def print_roller(request: RolerRequest) -> Response:
+    if config['ENV'] == 'dev':
+        return Response(ok=True, message='Process Finished', data={})
     return controller.print_rol(request)
 
 
 @router.post("/print-nalsani-rol")
 async def print_roller_nalsani(request: OrderDetailEanRequest) -> Response:
+    if config['ENV'] == 'dev':
+        return Response(ok=True, message='Process Finished', data={})
     return controller.print_order_nalsani_detail(request)
 
 
 @router.post("/print-reymond-rol")
-async def print_roller_nalsani(request: OrderDetailEanRequest) -> Response:
+async def print_roller_reymond(request: OrderDetailEanRequest) -> Response:
+    if config['ENV'] == 'dev':
+        return Response(ok=True, message='Process Finished', data={})
     return controller.print_order_reymond_detail(request)
 
 
 @router.post("/print-eliot-rol")
 async def print_roller_eliot(request: OrderDetailEanRequest) -> Response:
+    if config['ENV'] == 'dev':
+        return Response(ok=True, message='Process Finished', data={})
     return controller.print_order_eliot_detail(request)
